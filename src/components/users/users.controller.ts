bf6980241d7389a0483bdc96037c553f5c85770a
iv <consmebius@gmail.com>
2022-08-29 20:51:15 +0300

    add rep

--- src/components/users/users.controller.ts
+++ src/components/users/users.controller.ts
@@ -1,42 +1,42 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './users.service';
 
 @Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-  listlist;
+export class UserController {
+  constructor(private readonly service: UserService) {}
+
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body() body: CreateUserDto) {
+    return this.service.create(body);
   }
 
   @Get()
   list() {
-    return this.usersService.list();
+    return this.service.list();
   }
 
   @Get(':id')
   get(@Param('id') id: string) {
-    return this.usersService.get(+id);
+    return this.service.get(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.service.update(+id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.service.remove(+id);
   }
 }
--- src/components/users/users.module.ts
+++ src/components/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { DatabaseModule } from 'src/db/db.module';
+import { userProviders } from 'src/db/repository';
+import { UserController } from './users.controller';
+import { UserService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [DatabaseModule],
+  controllers: [UserController],
+  providers: [...userProviders, UserService],
 })
-export class UsersModule {}
+export class UserModule {}
